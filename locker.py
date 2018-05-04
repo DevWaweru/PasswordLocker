5f96fd7ea54e76d39598baa7b8af0b190d607517
Waweru <developer.waweru@gmail.com>
2018-05-04 19:13:22 +0300

    Adding the Credentials class and defining the init and create accoung functions

--- locker.py
+++ locker.py
@@ -1,5 +1,21 @@
 class Credentials:
-    my_user = True
+    '''
+    Class that creates accounts and authenticates the users
+    '''
+    users_list=[]
 
-class UserData:
-    print(Credentials.my_user)
+    def __init__(self,id,user_name,password):
+        '''
+        Initalizing the variables
+        '''
+        self.id = id,
+        self.user_name = user_name
+        self.password = password
+
+    def create_account(self):
+        '''
+        creating and saving log in credentials for the various users
+        '''
+        Credentials.users_list.append(self)
+        
+# class UserData:
