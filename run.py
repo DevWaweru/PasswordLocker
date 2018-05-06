7f2875bdfb638eab53b5e5982304053d646d47de
Waweru <developer.waweru@gmail.com>
2018-05-06 15:10:46 +0300

    Adding aesthetic changes on appearance

--- run.py
+++ run.py
@@ -61,7 +61,7 @@ def main():
         print("Type:\n  cc to create new account\n  ss to sign in\n  ex to exit")
         welcome_text = input().lower()
         if welcome_text == "cc":
-            print("Create account to continue: \n Enter Username")
+            print("Create account to continue:"+"\n"+"-"*25 + " \n Enter Username:")
             my_username = input()
             print(" Enter password:")
             my_password = input()
@@ -69,26 +69,31 @@ def main():
             print("\n")
             create_user(new_account(my_id,my_username,my_password))
             my_id+=1
-            print(f"User {my_username} has been created. Login to continue..")
+            print(f"User {my_username} has been created.\nLogin to continue")
             entries.append(0)
-            print("-"*20)
+            print("-"*27)
 
         elif welcome_text == "ss".lower():
             print("Enter username and password to continue:")
+            print("-"*40)
             print("Username: ")
             my_login = input()
             print("Password: ")
             my_key = input()
             get_result = authenticate(my_login,my_key)
             if get_result == 0:
+                print("\n")
                 print("Invalid username and/or password")
+                print("-"*27)
             elif get_result!=0:
-                print(f"{get_result.identify} has logged in")
+                # print(f"{get_result.identify} has logged in")
+                print("\n")
                 print(f"Welcome {get_result.user_name}! What would you like to do?")
                 while True:
                     print("Type:\n  ad - Add Password\n  vp - View Passwords\n  lo - Log Out")
                     get_input = input().lower()
                     if get_input == "ad":
+                        print("Add website and password to store them securely:")
                         print("Enter Website:")
                         my_website = input()
                         print("Generate password")
@@ -97,23 +102,31 @@ def main():
                         my_ident = get_result.identify
                         add_data(my_new_data(my_ident,entries[my_ident],my_website,my_webkey))
                         entries[my_ident]=entries[my_ident]+1
-                        print(f"Password on {my_website} has been created")
-                        print(f"This is the {entries[my_ident]} entry")
+                        print("\n")
+                        print(f"***Password on {my_website} has been created***")
+                        # print(f"This is the {entries[my_ident]} entry")
+                        print("-"*45)
 
                     elif get_input == "vp":
                         if data_existing(get_result.identify):
-                            print("Your passwords are: \n")
                             length = entries[get_result.identify]
-                            print(length)
+                            print(f"You have {length} passwords:")
+                            print("\n")
                             data_my=0
                             while data_my < length:
                                 get_password = display_data(get_result.identify,data_my)
-                                print(f"{get_password.website} ---- {get_password.web_key}")
+                                print(f"{data_my+1}. {get_password.website} ---- {get_password.web_key}")
                                 data_my+=1
+                            print("\nEnter a command to continue")
+                            print("-"*20)
                         else:
-                            print("No data exists on this user")
+                            print("\nYou have no data.\nType ad to add some passwords")
+                            print("-"*20)
 
                     elif get_input == "lo":
+                        print("\n")
+                        print(f"Goodbye {get_result.user_name}!")
+                        print("-"*30)
                         break
                     
                     else:
@@ -124,7 +137,6 @@ def main():
             break
 
         else:
-            print("\n")
             print("Invalid entry. Enter command again")
             print("\n"+"-"*40)
 
